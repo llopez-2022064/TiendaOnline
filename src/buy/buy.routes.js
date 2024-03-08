bdf761fa023e0906cf1647ee8f44b4cfe3f4ea7d
jguzman-2019642 <jguzman-2019642@kinal.edu.gt>
2024-03-08 17:18:15 -0600

    Entrega del proyecto final de taller

---
+++ src/buy/buy.routes.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import {Router} from 'express'
+import { addBuy ,ListarHistorialDeCompras} from './buy.controller.js'
+import { isAdmin, validateJwt } from '../middlewares/validate-jwt.js'
+
+const api = Router()
+
+//ADMIN
+api.get('/getFactura', [validateJwt], addBuy)
+api.get('/ListarHistorialDeCompras', [validateJwt], ListarHistorialDeCompras)
+
+export default api
